1794a2bc93eb0692412c1ac513ea9831b4dbe36a
Guilherme da Silva Martins Loureiro <1171056@isep.ipp.pt>
2019-11-30 21:56:07 +0000

    Entrance Animations

---
+++ Assets/Scripts/FightIntro.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FightIntro : MonoBehaviour
+{
+
+    private Animation fightLogoAnimation;
+
+    private Sprog sprog;
+    private Frolien frolian;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fightLogoAnimation = GameObject.Find("FightImage").GetComponent<Animation>();
+        sprog = GameObject.FindObjectOfType<Sprog>();
+        frolian = GameObject.FindObjectOfType<Frolien>();
+
+        sprog.gameObject.SetActive(false);
+        frolian.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!fightLogoAnimation.isPlaying) {
+
+            sprog.gameObject.SetActive(true);
+            frolian.gameObject.SetActive(true);
+            Destroy(this.gameObject);
+        }
+    }
+}
