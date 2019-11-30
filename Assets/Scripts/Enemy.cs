20b308c25c27061708324c96b8a2c6f81e9c59e4
GambuzX <mg_mesquita@hotmail.com>
2019-11-30 13:31:37 +0000

    Shooting projectiles

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,6 +6,10 @@ public class Enemy : MonoBehaviour
 {
 
     [SerializeField] float moveSpeed = 1f;
+    [SerializeField] float fireRate = 1f;
+
+    public GameObject projectilePrefab;
+    private bool shootLock;
 
     private Rigidbody2D rb;
 
@@ -13,29 +17,37 @@ public class Enemy : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        shootLock = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!shootLock && Input.GetButton("EnemyFire")) {
+            SpawnProjectile();
+        }
     }
 
     void FixedUpdate() {
         float horizontal = Input.GetAxis("HorizontalEnemy");
-        float vertical = Input.GetAxis("VerticalEnemy");
-
-        //print(horizontal);
-        //print(vertical);
+        float vertical = Input.GetAxis("VerticalEnemy"); 
 
         rb.velocity = new Vector2(horizontal * moveSpeed * Time.deltaTime, vertical * moveSpeed * Time.deltaTime);
     }
 
-    void OnCollisionStay2D(Collision2D collision) {
-        print("collision");
+    void SpawnProjectile() {
+
+        shootLock = true;
+
+        Vector2 direction = new Vector2(0f, 1f);
+
+        GameObject projectile = Instantiate(projectilePrefab, this.transform.position, Quaternion.identity);
+        projectile.GetComponent<Rigidbody2D>().velocity = direction;
+
+        Invoke("UnlockShoot", fireRate);
     }
 
-    void OnTriggerStay2D(Collider2D collider) {
-        print("trigger");
+    void UnlockShoot() {
+        shootLock = false;
     }
 }
---
+++ Assets/Scripts/EnemyProjectile.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyProjectile : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("SelfDestruct", 8f);
+    }
+    
+    void SelfDestruct() {
+        Destroy(this.gameObject);
+    }
+}
