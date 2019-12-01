84cccc720c9e46259bc8851c94331f36e0a066c0
Pedro Esteves <up201705160@fe.up.pt>
2019-12-01 16:04:43 +0000

    Added Screen Wrapper

---
+++ Assets/Scripts/ScreenWrapper.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenWrapper : MonoBehaviour
+{
+    float leftConstraint = Screen.width;
+    float rightConstraint = Screen.width;
+    float bottomConstraint = Screen.height;
+    float topConstraint = Screen.height;
+    float buffer = 1.0f;
+    Camera cam;
+    float distanceZ;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+        buffer = this.gameObject.GetComponentInChildren<SpriteRenderer>().bounds.size.x/2.0f;
+        distanceZ = Mathf.Abs(cam.transform.position.z + transform.position.z);
+        leftConstraint = cam.ScreenToWorldPoint(new Vector3(0.0f, 0.0f, distanceZ)).x;
+        rightConstraint = cam.ScreenToWorldPoint(new Vector3(Screen.width, 0.0f, distanceZ)).x;
+        bottomConstraint = cam.ScreenToWorldPoint(new Vector3(0.0f, 0.0f, distanceZ)).y;
+        topConstraint = cam.ScreenToWorldPoint(new Vector3(0.0f, Screen.height, distanceZ)).y;
+    }
+
+    private void FixedUpdate()
+    {
+        if(transform.position.x < leftConstraint - buffer)
+        {
+            transform.position = new Vector3(rightConstraint+buffer, transform.position.y, transform.position.z);
+        }
+
+        if (transform.position.x > rightConstraint + buffer)
+        {
+            transform.position = new Vector3(leftConstraint-buffer, transform.position.y, transform.position.z);
+        }
+    }
+}
